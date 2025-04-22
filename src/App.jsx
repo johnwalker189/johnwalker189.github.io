10caa3a8206b47f8814b12ae4b2e6e52d9a571b0
johnwalker189 <bang.nq224826@sis.hust.edu.vn>
2025-04-22 23:14:27 +0700

    [BIG UPDATE] Upload background, adding main pages.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from './components/Navbar';
+import HeroSection from './components/BackgroundVideo';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection>
+        <h1 className="text-lg font-bold text-center mx-auto max-w-3xl text-white animate-pulse">
+          Welcome to my stupid place~
+        </h1>
+      </HeroSection>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <nav className="p-4 w-full fixed top-0 left-0 z-50">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* Brand Name */}
+                <a href="/" className="text-white text-lg font-semibold hover:text-gray-500">Home
+                </a>
+
+                {/* Hamburger Menu for mobile */}
+                <button onClick={() => setIsOpen(!isOpen)}
+                    className="text-white focus:outline-none md:hidden">
+                    {/* Hamburger Icon and Close Icon */}
+                    {isOpen ? (
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    ) : (
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h16m-7 6h7"
+                            />
+                        </svg>
+                    )}
+                </button>
+
+                {/* Navigation Links */}
+                <div
+                    className={`w-full md:flex md:items-center md:w-auto 
+                    md:space-x-4 fixed md:relative top-16 left-0 md:top-0 
+                    md:left-0 p-4 md:p-0 bg-green-600 md:bg-transparent 
+                    transition-all duration-500 ease-in-out transform ${isOpen ? 
+                    'translate-x-0' : 'translate-x-full'
+                        } md:translate-x-0`}>
+                    <a  href="/about"
+                        className="block py-2 px-4 text-white hover:text-gray-500
+                                   md:inline-block">About Me
+                    </a>
+                    <a  href="/repositories"
+                        className="block py-2 px-4 text-white hover:text-gray-500
+                                   md:inline-block">My Dump
+                    </a>
+                    <a  href="/contact"
+                        className="block py-2 px-4 text-white hover:text-gray-500
+                                   md:inline-block">Contact
+                    </a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
