291e8a5d9ceb593c76aec96cb8b4f60c17fcbd08
johnwalker189 <bang.nq224826@sis.hust.edu.vn>
2025-04-23 01:34:37 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Navbar from './components/Navbar';
+import HeroSection from './components/BackgroundVideo';
 import './index.css'
 import App from './App.jsx'
 
