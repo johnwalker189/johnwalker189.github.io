f1b5b742a5b2d8a6bda10fb316b05194c641c780
johnwalker189 <bang.nq224826@sis.hust.edu.vn>
2025-04-23 01:27:57 +0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Navbar from './components/Navbar';
 import HeroSection from './components/BackgroundVideo';
 
@@ -5,11 +6,11 @@ function App() {
   return (
     <>
       <Navbar />
-      {/*<HeroSection>
+      <HeroSection>
         <h1 className="text-lg font-bold text-center mx-auto max-w-3xl text-white animate-pulse">
           Welcome to my stupid place~
         </h1>
-      </HeroSection>*/}
+      </HeroSection>
     </>
   );
 }
