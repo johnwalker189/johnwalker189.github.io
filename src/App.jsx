3495615377822c687f83b1bef1fc8d2c06fae68e
johnwalker189 <bang.nq224826@sis.hust.edu.vn>
2025-04-23 10:15:57 +0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar';
-import HeroSection from './components/BackgroundVideo';
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1 className="text-lg font-bold text-center mx-auto max-w-3xl text-white animate-pulse">
-        Welcome to my stupid place~
-      </h1>
+      <div className="h-screen flex items-center justify-center bg-black">
+        <h1 className="text-lg font-bold text-center mx-auto max-w-3xl text-white animate-pulse">
+          Welcome to my stupid place~
+        </h1>
+      </div>
     </>
   );
 }
