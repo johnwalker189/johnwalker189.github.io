fae1298059907c0437fbefc33fec5d1f4f45861e
johnwalker189 <bang.nq224826@sis.hust.edu.vn>
2025-04-23 10:36:22 +0700

    Update WelcomeSection.jsx

--- src/components/WelcomeSection.jsx
+++ src/components/WelcomeSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const WelcomeSection = () => {
   return (
-    <div className="h-screen flex items-center justify-center bg-black">
+    <div className="h-screen flex items-center justify-center bg-black pt-16">
       <h1 className="text-lg font-bold text-center mx-auto max-w-3xl text-white animate-pulse">
         Welcome to my stupid place~
       </h1>
