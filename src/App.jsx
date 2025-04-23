8123eefbfaea6ab622d91b07b2dfa55f9f87182f
johnwalker189 <bang.nq224826@sis.hust.edu.vn>
2025-04-23 10:13:07 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,9 @@ function App() {
   return (
     <>
       <Navbar />
-      <HeroSection>
-        <h1 className="text-lg font-bold text-center mx-auto max-w-3xl text-white animate-pulse">
-          Welcome to my stupid place~
-        </h1>
-      </HeroSection>
+      <h1 className="text-lg font-bold text-center mx-auto max-w-3xl text-white animate-pulse">
+        Welcome to my stupid place~
+      </h1>
     </>
   );
 }
