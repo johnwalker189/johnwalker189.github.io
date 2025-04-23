40df1729827c4149d0ece8b52395a32ff40994a2
johnwalker189 <bang.nq224826@sis.hust.edu.vn>
2025-04-23 10:22:01 +0700

    Make welcome page into a component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar';
-
+import WelcomeSection from './components/WelcomeSection';
 function App() {
   return (
     <>
       <Navbar />
-      <div className="h-screen flex items-center justify-center bg-black">
-        <h1 className="text-lg font-bold text-center mx-auto max-w-3xl text-white animate-pulse">
-          Welcome to my stupid place~
-        </h1>
-      </div> 
+      <WelcomeSection />
     </>
   );
 }
---
+++ src/components/WelcomeSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const WelcomeSection = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-lg font-bold text-center mx-auto max-w-3xl text-white animate-pulse">
+        Welcome to my stupid place~
+      </h1>
+    </div>
+  );
+};
+
+export default WelcomeSection;
